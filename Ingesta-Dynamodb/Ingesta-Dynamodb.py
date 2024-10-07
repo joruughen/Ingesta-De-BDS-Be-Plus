8e9fed2879a442fedf09b30b7d1c9beb4b947212
Jorge Eduardo Melgarejo Castillo <jorge.melgarejo@utec.edu.pe>
2024-10-06 21:38:19 -0500

    mm

--- Ingesta-Dynamodb/Ingesta-Dynamodb.py
+++ Ingesta-Dynamodb/Ingesta-Dynamodb.py
@@ -2,43 +2,35 @@ import boto3
 import json
 from decimal import Decimal
 
-# Configuración de DynamoDB y S3
-nombreTabla = "Products"  # Nombre de la tabla en DynamoDB
-nombreBucket = "bucket-ingesta-parcial"  # Nombre del bucket en S3
-ficheroUpload = "data.json"  # Nombre del archivo JSON a subir a S3
+nombreTabla = "Products"
+nombreBucket = "bucket-ingesta-parcial"
+ficheroUpload = "data.json"
 
-# Conectar a DynamoDB (usa el endpoint si es DynamoDB local)
 dynamodb = boto3.resource('dynamodb', endpoint_url="http://3.230.28.178:8000", region_name="us-west-1")
 table = dynamodb.Table(nombreTabla)
 
-# Función para obtener los datos de DynamoDB
 def obtener_datos_dynamodb():
-    response = table.scan()  # Realiza un scan para obtener todos los elementos
+    response = table.scan()
     data = response['Items']
 
-    # Manejar paginación si hay más elementos que los retornados en el primer scan
     while 'LastEvaluatedKey' in response:
         response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
-        data.extend(response['Items'])  # Agregar los nuevos elementos
+        data.extend(response['Items'])
 
     return data
 
-# Función personalizada para manejar objetos de tipo Decimal
 def decimal_default(obj):
     if isinstance(obj, Decimal):
-        return float(obj)  # Convertir Decimal a float
+        return float(obj)
     raise TypeError
 
-# Obtener los datos de DynamoDB
 data = obtener_datos_dynamodb()
 
-# Guardar los datos en un archivo JSON localmente
 with open(ficheroUpload, 'w') as file:
-    json.dump(data, file, indent=4, default=decimal_default)  # Usar la función para convertir Decimals
+    json.dump(data, file, indent=4, default=decimal_default)
 
 print(f"Datos exportados a {ficheroUpload} correctamente.")
 
-# Subir el archivo JSON al bucket de S3
 s3 = boto3.client('s3')
 
 try:
